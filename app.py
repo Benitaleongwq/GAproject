4fa9949128de1c133b6788b09fd8e9d9672e7e6a
Benitaleongwq <benitaleongwq@gmail.com>
2024-07-04 14:30:12 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from sklearn.ensemble import RandomForestClassifier
 st.set_page_config(page_title='Predict Flower Species', page_icon='🌷', layout='wide', initial_sidebar_state='expanded')
 
 # Set title of the app
-st.title('🌷 Predict Flower Species')
+st.title('Stock Prediction')
 
 # Load data
 df = pd.read_csv('iris.csv')
